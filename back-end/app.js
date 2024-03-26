1ff9513fb6e0318f9b6845dd388fbaf8b81969e2
Helium Yang <helium@heliumyang.com>
2024-03-26 05:28:51 +0000

    update

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 2999
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  	//res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
+	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
